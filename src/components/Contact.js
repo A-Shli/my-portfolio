006fd1e11176dc804229e7ee018e07c1e793b81e
UP2026661 <up2026661@myport.ac.uk>
2025-01-22 20:34:17 +0000

    updated linkedin link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     <section id="contact">
       <h1>Contact Me</h1>
       <p><FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:ala.shlibak07@gmail.com">ala.shlibak07@gmail.com</a></p>
-      <p><FontAwesomeIcon icon={faLinkedin} /> <a href="https://www.linkedin.com/in/yourprofile" target="_blank">Connect with me on LinkedIn</a></p>
+      <p><FontAwesomeIcon icon={faLinkedin} /> <a href="https://www.linkedin.com/in/ala-shlibak" target="_blank">Connect with me on LinkedIn</a></p>
       <p><FontAwesomeIcon icon={faGithub} /> <a href="https://github.com/A-Shli" target="_blank">Check out my GitHub</a></p>
     </section>
   );
